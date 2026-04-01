c77d1b0bde62d6035ff5143efb219d016231ba55
liviapaula-ux <livia.paula@ifood.com.br>
2026-04-01 16:14:08 -0300

    save final funcionando

--- api/save.js
+++ api/save.js
@@ -6,14 +6,23 @@ export default async function handler(req, res) {
   try {
     const data = req.body || {};
 
-    const params = new URLSearchParams();
-
-    Object.keys(data).forEach(key => {
-      params.append(key, data[key] || '');
-    });
+    const nome = encodeURIComponent(data.nome || '');
+    const email = encodeURIComponent(data.email || '');
+    const peso = encodeURIComponent(data.peso || '');
+    const altura = encodeURIComponent(data.altura || '');
+    const estadoCivil = encodeURIComponent(data.estadoCivil || '');
+    const dpp = encodeURIComponent(data.dpp || '');
+    const dum = encodeURIComponent(data.dum || '');
+    const idadeGestacional = encodeURIComponent(data.idadeGestacional || '');
+    const primeiraGestacao = encodeURIComponent(data.primeiraGestacao || '');
+    const tipoGestacao = encodeURIComponent(data.tipoGestacao || '');
+    const historicoClinico = encodeURIComponent(data.historicoClinico || '');
+    const diagnosticos = encodeURIComponent(data.diagnosticos || '');
+    const medicamentos = encodeURIComponent(data.medicamentos || '');
+    const intercorrencias = encodeURIComponent(data.intercorrencias || '');
 
     const scriptURL =
-      'https://script.google.com/macros/s/AKfycbwxxKwC6QorFSKIjSGjRXprfXMaTHw8cL2WiRnr0vNgG5FR1PWYBfdW5mbLuTy5cU5g/exec?' + params.toString();
+      `https://script.google.com/macros/s/AKfycbwxxKwC6QorFSKIjSGjRXprfXMaTHw8cL2WiRnr0vNgG5FR1PWYBfdW5mbLuTy5cU5g/exec?nome=${nome}&email=${email}&peso=${peso}&altura=${altura}&estadoCivil=${estadoCivil}&dpp=${dpp}&dum=${dum}&idadeGestacional=${idadeGestacional}&primeiraGestacao=${primeiraGestacao}&tipoGestacao=${tipoGestacao}&historicoClinico=${historicoClinico}&diagnosticos=${diagnosticos}&medicamentos=${medicamentos}&intercorrencias=${intercorrencias}`;
 
     const response = await fetch(scriptURL);
     const text = await response.text();
