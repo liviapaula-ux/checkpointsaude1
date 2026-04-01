20a187198ebb5800d182d632c62cd2f0c5d7b1c2
liviapaula-ux <livia.paula@ifood.com.br>
2026-04-01 16:04:28 -0300

    Update save.js

--- api/save.js
+++ api/save.js
@@ -1,26 +1,17 @@
 export default async function handler(req, res) {
-  const data = req.body;
-
-  const params = new URLSearchParams(data).toString();
-
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwxxKwC6QorFSKIjSGjRXprfXMaTHw8cL2WiRnr0vNgG5FR1PWYBfdW5mbLuTy5cU5g/exec?' + params;
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 
   try {
+    const data = req.body;
+    const params = new URLSearchParams(data).toString();
+
+    const scriptURL =
+      'https://script.google.com/macros/s/AKfycbwxxKwC6QorFSKIjSGjRXprfXMaTHw8cL2WiRnr0vNgG5FR1PWYBfdW5mbLuTy5cU5g/exec?' + params;
+
     const response = await fetch(scriptURL);
     const text = await response.text();
-
-    return res.status(200).json({
-      success: true,
-      googleResponse: text
-    });
-
-  } catch (error) {
-    return res.status(500).json({
-      success: false,
-      error: error.message
-    });
-  }
-}
 
     return res.status(200).json({
       success: true,
